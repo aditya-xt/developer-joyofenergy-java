3f9b152b09a781e2ff3933f2a6d37e9837c9f742
aditya-xt <singh.aditya3791@gmail.com>
2025-05-01 23:29:27 +0530

    New Method for debuging

--- src/main/java/uk/tw/energy/controller/CostController.java
+++ src/main/java/uk/tw/energy/controller/CostController.java
@@ -1,6 +1,8 @@
 package uk.tw.energy.controller;
 
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +17,8 @@ import uk.tw.energy.domain.ElectricityReading;
 import uk.tw.energy.service.MeterReadingService;
 import uk.tw.energy.service.PricePlanService;
 
+import javax.xml.transform.Result;
+
 @RestController
 @RequestMapping("/getCost")
 public class CostController {
@@ -27,8 +31,9 @@ public class CostController {
 
     @GetMapping("/getCostOfLastWeek/{smartMeterID}")
     public ResponseEntity<String> getCostOfLastWeek(@PathVariable String smartMeterID) {
-        Double avg = getAverage(smartMeterID);
-        return new ResponseEntity<>(avg.toString(), HttpStatus.OK);
+//        Double avg = getAverage(smartMeterID);
+        String avg = getAverageString(smartMeterID);
+        return new ResponseEntity<>(avg, HttpStatus.OK);
     }
 
     private Double getAverage(String smartMeterID) {
@@ -45,5 +50,33 @@ public class CostController {
         }
         avg = avg != null ? avg : 0 ;
         return avg;
+    }
+
+    private String getAverageString(String smartMeterID) {
+        Optional<List<ElectricityReading>> readingsOptional = meterReadingService.getReadings(smartMeterID);
+        ArrayList<ElectricityReading> electricityReadings = new ArrayList<>();
+        readingsOptional.ifPresent(electricityReadings::addAll);
+        Double avg = null;
+        String result = "";
+        Instant instant = Instant.now();
+        Instant instant1 = Instant.now().minus(5, ChronoUnit.DAYS);
+        if (!electricityReadings.isEmpty()) {
+            BigDecimal bigDecimal = electricityReadings.stream()
+                    .map(ElectricityReading::reading)
+                    .reduce(BigDecimal::add)
+                    .get();
+            avg = Double.parseDouble(bigDecimal.toString()) / electricityReadings.size();
+
+            result = "{" +
+                    "readingsOptional : "+readingsOptional+"\n"+
+                    "bigDecimal : "+bigDecimal+"\n"+
+                    "avg : "+avg+"\n"+
+                    "electricityReadings : "+electricityReadings+"\n"+
+                    "instant now : "+instant+"\n"+
+                    "instant1 7 days ago date : "+instant1+"\n"+
+                    "}";
+        }
+        avg = avg != null ? avg : 0 ;
+        return result;
     }
 }
