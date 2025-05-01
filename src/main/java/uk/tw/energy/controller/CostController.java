ed18f446f5dcde5d62dde316f9a94072fb826dda
aditya-xt <singh.aditya3791@gmail.com>
2025-05-01 20:48:34 +0530

    applied spotless on Cost Controller and added a new constructor

--- src/main/java/uk/tw/energy/controller/CostController.java
+++ src/main/java/uk/tw/energy/controller/CostController.java
@@ -1,5 +1,9 @@
 package uk.tw.energy.controller;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,26 +14,30 @@ import uk.tw.energy.domain.ElectricityReading;
 import uk.tw.energy.service.MeterReadingService;
 import uk.tw.energy.service.PricePlanService;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/getCost")
 public class CostController {
 
-    @Autowired
     private MeterReadingService meterReadingService;
-    @Autowired
+
     private PricePlanService pricePlanService;
 
+    public CostController(MeterReadingService meterReadingService, PricePlanService pricePlanService) {
+        this.meterReadingService = meterReadingService;
+        this.pricePlanService = pricePlanService;
+    }
+
     @GetMapping("/getCostOfLastWeek/{smartMeterID}")
-    public ResponseEntity<String> getCostOfLastWeek(String smartMeterID){
+    public ResponseEntity<String> getCostOfLastWeek(String smartMeterID) {
         Optional<List<ElectricityReading>> readingsOptional = meterReadingService.getReadings(smartMeterID);
         ArrayList<ElectricityReading> electricityReadings = new ArrayList<>();
         readingsOptional.ifPresent(electricityReadings::addAll);
-        BigDecimal bigDecimal = !electricityReadings.isEmpty() ? electricityReadings.stream().map(ElectricityReading::reading).reduce(BigDecimal::add).get() : BigDecimal.ZERO;
-        return new ResponseEntity<>(bigDecimal.toString(),HttpStatus.OK);
+        BigDecimal bigDecimal = !electricityReadings.isEmpty()
+                ? electricityReadings.stream()
+                        .map(ElectricityReading::reading)
+                        .reduce(BigDecimal::add)
+                        .get()
+                : BigDecimal.ZERO;
+        return new ResponseEntity<>(readingsOptional.toString(), HttpStatus.OK);
     }
 }
