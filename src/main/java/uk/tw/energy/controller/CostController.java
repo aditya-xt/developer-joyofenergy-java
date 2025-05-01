e5e021e83297d11d7080f11890a6acfbb9779fcb
aditya-xt <singh.aditya3791@gmail.com>
2025-05-01 20:38:36 +0530

    Created Cost Controller

---
+++ src/main/java/uk/tw/energy/controller/CostController.java
@@ -0,0 +1,35 @@
+package uk.tw.energy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.tw.energy.domain.ElectricityReading;
+import uk.tw.energy.service.MeterReadingService;
+import uk.tw.energy.service.PricePlanService;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/getCost")
+public class CostController {
+
+    @Autowired
+    private MeterReadingService meterReadingService;
+    @Autowired
+    private PricePlanService pricePlanService;
+
+    @GetMapping("/getCostOfLastWeek/{smartMeterID}")
+    public ResponseEntity<String> getCostOfLastWeek(String smartMeterID){
+        Optional<List<ElectricityReading>> readingsOptional = meterReadingService.getReadings(smartMeterID);
+        ArrayList<ElectricityReading> electricityReadings = new ArrayList<>();
+        readingsOptional.ifPresent(electricityReadings::addAll);
+        BigDecimal bigDecimal = !electricityReadings.isEmpty() ? electricityReadings.stream().map(ElectricityReading::reading).reduce(BigDecimal::add).get() : BigDecimal.ZERO;
+        return new ResponseEntity<>(bigDecimal.toString(),HttpStatus.OK);
+    }
+}
